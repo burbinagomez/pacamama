5a8c75ee97dbb70b4eb4dfab18075dacc7abdef0
Brayan Urbina <burbinagomez@gmail.com>
2022-07-19 09:02:44 -0500

    Update calendar

--- pages/events.vue
+++ pages/events.vue
@@ -1,10 +1,8 @@
 <template>
   <div>
-    <Header />
     <PageHeader pageTitle="Events" pageName="Events" />
-    <EventsGrid />
-    <Footer />
-    <SearchPopup />
-    <MobileDrawer />
+    <iframe 
+  src="https://calendar.google.com/calendar/embed?src=c_classroom0e5eebda%40group.calendar.google.com&ctz=America%2FBogota" 
+  style="border: 0" width="100%" height="600" frameborder="0" scrolling="no"></iframe>
   </div>
 </template>
